d75122a3fcb1c80db749314323a54f69fe07b75a
Peter Kordel <pkordel@gmail.com>
2022-04-11 10:34:48 +0700

    Set time zone

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Medix
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Stockholm"
     config.eager_load_paths << Rails.root.join("lib")
 
     # Don't generate system test files.
