b773b1b7f812ab5e44db737773dda31667be8c73
Peter Kordel <pkordel@gmail.com>
2022-04-11 16:15:24 +0700

    Start search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @professionals = if params[:query].present?
+      Profile.where("full_name ILIKE ?", "%#{params[:query]}%")
+    else
+      Profile.all
+    end
   end
 end
