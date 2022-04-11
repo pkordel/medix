cef401237ee18c970365b33ed2bb09eecab0c843
Peter Kordel <pkordel@gmail.com>
2022-04-11 18:30:10 +0700

    Simple search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,17 @@
 class HomeController < ApplicationController
+  before_action :turbo_frame_request_variant
+
   def index
-    @professionals = if params[:query].present?
+    @profiles = if params[:query].present?
       Profile.where("full_name ILIKE ?", "%#{params[:query]}%")
     else
-      Profile.all
+      Profile.none
     end
+  end
+
+  private
+
+  def turbo_frame_request_variant
+    request.variant = :turbo_frame if turbo_frame_request?
   end
 end
